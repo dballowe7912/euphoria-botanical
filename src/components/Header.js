bbb6318ffffc099706cbe16bed7a0f8fba853e5d
unknown <dballowe7912@gmail.com>
2021-09-29 13:45:53 -0500

    Replaced Icon, removed borders from tea list

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,9 +6,9 @@ function Header() {
             <div className="container h-100">
                 <div className="row h-100 align-items-center">
                     <div className="col-12 text-center">
-                    <h1 className="fw-light">Euphoria Botanical</h1>
+                    <h1 className="fw-light">EUPHORIA BOTANICAL</h1>
                     <p className="lead">We are here for all your botanical, medicinal & Skate Gear needs! </p>
-                    <button className="btn btn-secondary">Shop Now!</button>
+                    <button className="btn btn-success">Shop Now!</button>
                     </div>
                 </div>
             </div>
