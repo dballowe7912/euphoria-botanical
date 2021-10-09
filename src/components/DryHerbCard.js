80368d2943c7170f600d0e65aff6f9e7f6b7377e
unknown <dballowe7912@gmail.com>
2021-10-08 23:57:45 -0500

    Created flippable card for DryHerb page

--- src/components/DryHerbCard.js
+++ src/components/DryHerbCard.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import { Link }  from 'react-router-dom';
+import React, { useState } from 'react';
 
-function DryHerbCard(props) {
+function DryHerbCard({herbTitle, src, details}) {
+
+    const [flipped, setFlipped] = useState(false);
+
+    const handleClick = () => {
+        setFlipped(!flipped)
+    }
+
     return (
-        <Link to="/home" className="col-md-4 clickable-card">
-            <div className="card mb-4 box-shadow">
-                <img className="card-img-top card-img-style" 
-                    data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" 
-                    alt="Thumbnail [100%x225]" 
-                    src={props.src}
-                    data-holder-rendered="true"
-                />
-                <div className="card-body">
-                    <p className="card-text">{props.herbTitle}</p>
-                    <div className='card-button'>
-                        <button className="btn btn-success">Details and Pricing</button>                                
+        <div className="card-container col-md-4 clickable-card" onClick={handleClick}>
+            <div className={`card__inner ${flipped ? 'isFlipped' : ''}`}>
+                <div className=" card__face card__face--front">
+                    <div className="card mb-4 box-shadow">
+                        <p className="card-text">{herbTitle}</p>
+                        <img className="card-img-top card-img-style" 
+                            data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" 
+                            alt="Thumbnail [100%x225]" 
+                            src={src}
+                            data-holder-rendered="true"
+                        />
+                        <div className="card-body">
+                            <div className='card-button'>
+                                <button 
+                                    className="btn btn-success"
+                                    onClick={handleClick}
+                                >Details and Pricing</button>                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="clickable-card card__face card__face--back">
+                    <div className="card mb-4 box-shadow card__content">
+                        <img className="card-img-top card-img-style" 
+                            data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" 
+                            alt="Thumbnail [100%x225]" 
+                            src={src}
+                            data-holder-rendered="true"
+                        />
+                        <div className="card-body">
+                            <p className="card-text">{details}</p>
+                            <div className='card-button'>
+                                <button 
+                                    className="btn btn-success"
+                                    onClick={handleClick}
+                                >
+                                    Back
+                                </button>                                
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </Link>
+        </div>
     )
 }
 
