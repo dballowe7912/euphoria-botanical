56400042591dca9a9dc6f0522717dc90292ad79e
unknown <dballowe7912@gmail.com>
2021-10-10 07:33:21 -0500

    Added product pricing to back of flip cards on DryHerb page and added arrows at bottom

--- src/components/DryHerbCard.js
+++ src/components/DryHerbCard.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-function DryHerbCard({herbTitle, src, details}) {
+function DryHerbCard({herbTitle, src, details, priceData}) {
 
     const [flipped, setFlipped] = useState(false);
 
@@ -8,12 +8,18 @@ function DryHerbCard({herbTitle, src, details}) {
         setFlipped(!flipped)
     }
 
+    priceData.map((i) => {
+        console.log(i.type, i.size, i.price.toFixed(2))
+    })
+
+
     return (
         <div 
             onClick={handleClick}
             className="card-container col-md-4 clickable-card"
         >
             <div className={`card__inner ${flipped ? 'isFlipped' : ''}`}>
+
                 <div className=" card__face card__face--front">
                     <div className="card">
                         <p className="card-text custom-font card-title">{herbTitle}</p>
@@ -24,21 +30,35 @@ function DryHerbCard({herbTitle, src, details}) {
                             data-holder-rendered="true"
                         />
                         <div className="card-body">
-                        <p className="card-text custom-font card-summary">{details}</p>
-                            
+                            <p className="card-text custom-font card-summary">{details}</p>
+                            <span>
+                            <i class="fas fa-arrow-right card-flip-icon"></i>
+                            </span>
                         </div>
                     </div>
                 </div>
-                <div className="clickable-card card__face card__face--back">
-                    <div className="card mb-4 box-shadow card__content">
-                        <img className="card-img-top card-img-style" 
-                            data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" 
-                            alt="Thumbnail [100%x225]" 
-                            src={src}
-                            data-holder-rendered="true"
-                        />
+                    
+                <div className=" card__face card__face--back">
+                    <div className="card">
+                        <p className="card-text custom-font card-title">{herbTitle}</p>
+                        
                         <div className="card-body">
-                            
+                        <p className="card-text custom-font card-pricing">{}</p>
+                            <div className="row border-bottom" style={{margin: "auto"}}>
+                                <div className="col">Product</div>
+                                <div className="col text-center">Size</div>
+                                <div className="col text-right">Price</div>
+                            </div>
+                            {priceData.map((i) => (
+                                <div className="row" style={{margin: "auto"}}>
+                                    <div className="col py-1">{i.type}</div>
+                                    <div className="col text-center py-1">{i.size}</div>
+                                    <div className="col text-right py-1">${i.price.toFixed(2)}</div>
+                                </div>
+                        ))}
+                        <span>
+                            <i className="fas fa-undo card-flip-icon"></i>
+                        </span>
                         </div>
                     </div>
                 </div>
--- src/pages/DryHerbs.js
+++ src/pages/DryHerbs.js
@@ -35,6 +35,7 @@ function DryHerbs() {
         window.scrollTo(0, 0)
     }, [])
 
+    // console.log(dryHerbs[0].pricing.map((i) => console.log(i.type, i.size, i.price)))
 
     return (
     <div className="dry-herbs-page">
@@ -42,24 +43,27 @@ function DryHerbs() {
         <div className="album bg-light">
             <div className="container">
             <div className="row">
-                <h1 className="custom-font text-center page-title" style={{maxHeight: '10px', margin: 'auto'}}>Dry Herbs</h1>
+                <h1 className="custom-font text-center page-title">Dry Herbs</h1>
             </div>
                 <div className="row" style={{paddingTop: "0"}}>
                     <DryHerbCard 
                         src={akuamma} 
                         herbTitle="Akuamma / Picralima Nitida"
                         details="A part of the plant with the highest concentration of alkaloids. It gives a sense of well-being in combination with mild sedation. Effects typically last between 3 and 4 hours."
+                        priceData={dryHerbs[0].pricing}
                     />
                     <DryHerbCard 
                         src={blueLotus} 
                         herbTitle="Blue Lotus / Nymphaea Caerulea"
                         details= "Blue Lotus is well respected as a holistic substance for centuries, with its ability to enhance mediation and promote relaxation. It also has been found to have pain relieving properties that moderate pain and muscle stress in the body."
+                        priceData={dryHerbs[1].pricing}
                     />
 
                     <DryHerbCard 
                         src={catsClaw} 
                         herbTitle="Cats Claw / Uncaria Tomentosa"
                         details= "Cats Claw is derived from a tropical vine. Cats Claw helps support your immune system, possible helping fight infections more effectively."
+                        priceData={dryHerbs[2].pricing}
                     />
 
                 </div> 
@@ -70,18 +74,21 @@ function DryHerbs() {
                         src={damiana} 
                         herbTitle="Damiana / Turnera Diffusa"
                         details= "Damiana is used to treat headaches, bedwetting, depression, nervous stomach, and constipation; for prevention and treatment of sexual problems; boosting and maintaining mental and physical stamina; and as an aphrodisiac."
+                        priceData={dryHerbs[3].pricing}
                     />
 
                     <DryHerbCard 
                         src={horneyGoat} 
                         herbTitle="Horney Goat Weed / Epimedium Grandiflorum"
                         details= "Horney goat weed is a herb. The leaves are used to make medicine. Horney Goat Weed is commonly used by mouth for sexual performance problems, such as erectile dysfunction (ED) and low sexual desire. It is also for back and knees, joint pain, arthritis, mental and physical fatigue, and memory loss along with many other conditions."
+                        priceData={dryHerbs[4].pricing}
                     />
 
                     <DryHerbCard 
                         src={kanna} 
                         herbTitle="Kanna Shred / Sceletium Tortusom"
                         details= "Kanna elevates mood and decreases anxiety, stress and tension, and it has been used as an appetite suppressant. If you want to turn a bad day around, this beneficial tea is as reassuring to your state of mind as it is tasty."
+                        priceData={dryHerbs[5].pricing}
                     />
 
 
@@ -93,16 +100,19 @@ function DryHerbs() {
                         src={kava} 
                         herbTitle="Kava Extract / Piper Methysticum"
                         details= "Kava contains substances called kavapyrones. They act much like alcohol on your brain, making you feel calm, relaxed, and happy. The plant is also thought to relieve pain, seizures, and relax muscles. You can buy it as an herbal supplement online and in health food stores."
+                        priceData={dryHerbs[6].pricing}
                     />
                     <DryHerbCard 
                         src={dagga} 
                         herbTitle="Wild Dagga (Lions Tail) / Leonotis Leonurus"
                         details= "Wild Dagga mixtures are also consumed to help high blood pressure, jaundice, hemorrhoids, dysentery, fever, headaches, influenza, asthma, and coughs. Wild Dagga is commonly referred to as wild cannabis."
+                        priceData={dryHerbs[7].pricing}
                     />
                     <DryHerbCard 
                         src={wormwood} 
                         herbTitle="Wormwood / Artemisia"
                         details= "Wormwood is an herb that is native to Europe and North America. It is traditionaly used as a bitter tonic, carminative to support healthy appetite levels, and a healthy flora in the digestive tract. Wormwood is also used to treat fever, liver disease, depression, muscle pain, memory loss and worm infections."
+                        priceData={dryHerbs[8].pricing}
                     />
 
                 </div>
@@ -113,16 +123,19 @@ function DryHerbs() {
                         src={palo} 
                         herbTitle="Palo Azul - Detox Cheese"
                         details= "The bark of this plant has been used to create a potent cleansing tea for thousands of years. This detox clears toxins in the bladder, kidneys, and even removes traces of drug metabolites from urine, helping you pass a urinalysis."
+                        priceData={dryHerbs[9].pricing}
                     />
                     <DryHerbCard 
                         src={whiteWillow}
                         herbTitle="White Willow Bark Powder"
                         details= "The bark of white willow contains salicin, which is a chemical similar to aspirin. In combination with the herb's powerful anti-inflammatory plant compounds, salicin is thought to be responsible for the pain-relieving and anti-inflammatory effects of the herb."
+                        priceData={dryHerbs[10].pricing}
                     />
                     <DryHerbCard 
                         src={marshmallow} 
                         herbTitle="Marshmallow Root Powder / Althaea Officinalis"
                         details= "An herbal remedy used for multiple ailments; marshmallow root is the base of the marshmallow plant. Medically, the root may be used for anything from chapped skin to a common cold. Marshmallow root may also be used as a cosmetic ingredient. It can be used to treat bruises, to sooth a stomache ache, to treat gastritis, indigestion, as well as some coughs. The remedy has been used to treat Crohn's disease, asthma, diarrhea, and gastroesophageal reflux disease. This should be taken with at least 8oz of liquid. Consuming this product can expose you to lead, which is known in the state of California to cause birth defects of other reproductive harm."
+                        priceData={dryHerbs[11].pricing}
                     />
 
                 </div>
@@ -133,16 +146,19 @@ function DryHerbs() {
                         src={dreamHerb} 
                         herbTitle="Calea Zacatachichi / Calea Ternifolia"
                         details= "Medium sized shrub that is known to be a hallucinogen and to increase dreams. Because of this, it is often referred to as the 'dream herb'. It is also called 'bitter grass' because the plant material of calea zactachichi has an intense bitter taste. Calea zactachichi has been used in folk remedies for thousands of years as an appetite stimulant, cleansing agent, calming agent, laxative, and for the treatment of diarrhea, dysentery, fever, skin rashes, swollen scalps, and headaches. Some people use this herb to induce psychic dreams, help remember dreams, and to increase dreaming. This herb has also been studied for mental enchancement and sleep."
+                        priceData={dryHerbs[12].pricing}
                     />
                     <DryHerbCard 
                         src={ilex} 
                         herbTitle="Ilex Guayusa"
                         details= "Ilex Guayusa is a species of tree of the holly genus, native to the Amazon Rainforest. One of three known caffeinated holly trees, the leaves of the guayusa tree are dried and brewed like a tea for their stimulative effects. Guayusa leaves contain various beneficial compounds linked to potential health benefits. This Amazonian plant is fich in antioxidants and caffeine that may promote weight loss, blood sugar regulation, and improved mood and alertness. Its tea is safe to drink and serves as an excellent alternative to coffee and energy drinks by providing a healthier energy lift."
+                        priceData={dryHerbs[13].pricing}
                     />
                     <DryHerbCard 
                         src={lavender} 
                         herbTitle="Lavendar Petals / Lamianceae Lavendula"
                         details= "Lavender oil is believed to have anticeptic and anti-inflammatory properties, which can help to heal minor burns and bug bites. In addition to helping digestive problems, lavender is used to help relieve pain from headaches, sprains, toothaches, and sores. It can also be used to prevent hair loss."
+                        priceData={dryHerbs[14].pricing}
                     />
 
                 </div>
@@ -153,16 +169,19 @@ function DryHerbs() {
                         src={maitake} 
                         herbTitle="Maitake Mushroom"
                         details= "Maitake Mushroom is used to treat cancer and also to relieve some of the side effects of chemical treatment (chemotherapy) for cancer. It is also used for HIV/AIDS, chronic fatigue syndrome (CFS), hepatitis, hay fever, diabetes, high blood pressure, high cholesterol, weight loss, and infertility due to a condition known as polycystic ovary syndrome."
+                        priceData={dryHerbs[15].pricing}
                     />
                     <DryHerbCard 
                         src={matcha} 
                         herbTitle="Matcha Green Tea"
                         details= "Matcha is a type of green tea by taking young tea leaves and grinding them into a bright green powder. Matcha, like other green teas, contains a class of antioxidants called catechins. Matcha is high in catechins called EGCG (epigallocatechin gallate), which is believed to have cancer fighting effects on the body. Studies have linked green tea to a variety of health benefits, like helping prevent heart disease, type 2 diabetes, cancer, and even encouraging weight loss."
+                        priceData={dryHerbs[16].pricing}
                     />
                     <DryHerbCard 
                         src={reishi} 
                         herbTitle="Reishi Mushroom / Lingzhi"
                         details= "Reishi mushroom has been used to help enhance the immune system, reduce stress, improve sleep, and less fatigue. People also take Reishi for health conditions such as high blood pressure and high cholesterol."
+                        priceData={dryHerbs[17].pricing}
                     />
 
                 </div>
@@ -173,16 +192,19 @@ function DryHerbs() {
                         src={skullCap} 
                         herbTitle="Blue Skull Cap Leaf / Scutellaria Laterflora"
                         details= "Blue Skull cap is used for trouble sleeping (insomnia), anxiety, stroke, paralysis caused by stroke, as well as, fever and high cholesterol."
+                        priceData={dryHerbs[18].pricing}
                     />
                     <DryHerbCard 
                         src={tumeric} 
                         herbTitle="Tumeric Capsule / Tumeric Powder"
                         details= "Tumeric is a plant that is related to ginger, it is grown throughout India, parts of Asia, and Central America. It is commonly known as Indian saffron or the golden spice. Cercumin, the active ingredient in tumeric, is known for reducing the number of heart attacks bypass patients have after surgery, controlling knee pain from osteoarthritis, and reducing the skin irritation that often occurs after radiation treatments for breast cancer."
+                        priceData={dryHerbs[19].pricing}
                     />
                     <DryHerbCard 
                         src={valerian} 
                         herbTitle="Valerian / Valeriana Officinalis"
                         details= "The root of this flowering plant is dried and used as an herbal remedy. Valerian is most commonly used for sleep disorders such as insomnia. It is used orally for anxiety and psychological stress."
+                        priceData={dryHerbs[20].pricing}
                     />
 
                 </div>
@@ -193,6 +215,7 @@ function DryHerbs() {
                         src={whiteSage} 
                         herbTitle="White Sage"
                         details= "Burning White Sage is a ritual known for smudging. Smudging is a cleansing ceremony used to help people, places, or even objects to get rid of negative energy or even bad spirits."
+                        priceData={dryHerbs[21].pricing}
                     />
                 
                 </div>
