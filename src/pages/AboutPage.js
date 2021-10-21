b2a5b00e8df20b2d76c37fb3b7d3fa915931f66a
unknown <dballowe7912@gmail.com>
2021-10-21 09:39:58 -0500

    Edited about page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -9,7 +9,7 @@ const AboutPage = () => (
         <Navbar/>
         <div className=" about-main-container container" role="main">
             <h1 className="about-title">Euphoria Botanical</h1>
-            <p className="about-text text-center">Euphoria Botanical is an Herb Shop located downtown in Kilgore, Texas. We have a large selection of CBD flowers, oils, 
+            <p className="about-text">Euphoria Botanical is an Herb Shop located downtown in Kilgore, Texas. We have a large selection of CBD flowers, oils, 
                 salves and edibles available. We also serve the freshest herbs as well as a large variety of Kratom. Come check 
                 out our Essential Oils, Handmade Jewelry, Insence, Sage, Stones, Crystals, Handmade Soaps and even a Tea Lounge! Our 
                 local Euphoria Skate Shop offers Skate Clinics, Trejo Boards, Houston Skateboards and other skate gear and apparel.</p>
